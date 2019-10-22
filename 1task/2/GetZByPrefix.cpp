fbdc60fe69d9332c3557dacac58827da619b5401
Mikhail <44144091+nikolaev1ma@users.noreply.github.com>
2019-10-23 00:16:06 +0300

    Delete GetZByPrefix.cpp

--- 1task/2/GetZByPrefix.cpp
+++
@@ -1,8 +0,0 @@
-#include "GetStrByPrefix.cpp"
-#include "GetZByStr.cpp"
-#include "String.h"
-
-void String::get_z_by_prefix() {
-  get_str_by_prefix(); // сначала получаем строку по префикс-функции
-  get_z_by_str();      // получаем z-функцию по строке
-}
