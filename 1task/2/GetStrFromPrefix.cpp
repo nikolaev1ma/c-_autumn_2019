4ed2fc08c3a20fd957d2e2547bac908821f43933
Mikhail <nikolaev.ma@phystech.edu>
2019-10-31 01:32:12 +0300

    1.2

--- 1task/2/GetStrFromPrefix.cpp
+++ 1task/2/GetStrFromPrefix.cpp
@@ -31,7 +31,7 @@ private:
     }
     vector<bool> can_use_letter(
             size_alphabet); // can_use_letter[l] - может ли находиться буква
-    // l на позиции n
+                            // l на позиции n
     for (char letter = 'a'; letter <= 'z'; ++letter) {
       // изначально каждая буква может находиться на позиции n
       can_use_letter[letter - 'a'] = true;
